241fb6cf5d7d498750a46710ea9544f390f0b5a3
gerrardsarah <gerrardsarah@gmail.com>
2022-04-04 10:23:01 -0700

    change repo

---
+++ HTML, CSS, JS Practice - ToDoList/js/main.js
@@ -0,0 +1,17 @@
+import 'https://unpkg.com/open-props';
+import 'https://unpkg.com/open-props-css';
+
+
+// on click and enter key, add tasks written in input to the list, added to the dom
+
+// with pressing the trash can, remove tasks from list
+
+// upon pressing the checkbox, the task is completed
+
+// upon pressing the edit button, the task is editable
+
+// upon pressing the save or enter button, the task is saved
+
+// update pending tasks when a tasks is added or removed
+
+// clear all will remove all tasks from the list
